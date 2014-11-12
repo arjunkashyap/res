9663aae9ce81292d5f2636073360524a965b4c2f
Arjuna <arjun.kashyap@yahoo.co.in>
2014-11-12 22:54:13 +0530

    Commit No. 8 - Nov 2014 issue added

--- index.php
+++ index.php
@@ -100,7 +100,6 @@ $title_cv_display = preg_replace("/\<\/sub\>/", "", $title_cv_display);
 		<div class="ci_holder_mask">
 			<div class="ci_column1">
 				<?php print_widget("General Article", 1); ?>
-				<?php print_widget("Think It Over", 1); ?>
 				<?php print_widget("Series Article", 1); ?>
 				<?php print_widget("Information and Announcements", 1); ?>
 			</div>
@@ -113,6 +112,7 @@ $title_cv_display = preg_replace("/\<\/sub\>/", "", $title_cv_display);
 				<?php print_widget("Classroom", 1); ?>
 				<?php print_widget("Book Review", 1); ?>
 				<?php print_widget("Classics", 1); ?>
+				<?php print_widget("Think It Over", 1); ?>
 				<?php print_widget("Our Readers Write", 1); ?>
                 <?php print_widget("Feature Article", 1); ?>
 				<?php print_widget("Reflections", 1); ?>
--- php/connect.php
+++ php/connect.php
@@ -3,5 +3,5 @@ $user='root';
 $password='mysql';
 $database='res';
 $volume='19';
-$issue='10';
+$issue='11';
 ?>
